c7585f8d9703bb2ce4f1f6c34087cbe14eaf3945
corynorris <corynorris@gmail.com>
2020-11-07 16:56:55 -0500

    Added placeholder for plant behaviour

---
+++ Assets/Scripts/ICellBehaviour.cs
@@ -0,0 +1,10 @@
+// TBD
+public interface ICellBehaviour
+{
+    int GetPriority();
+
+    int CalculateNextSpawn();
+
+    void SpawnNextCell();
+
+}
---
+++ Assets/Scripts/Plants/ExpandPlant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandPlant : Plant
+{
+    public override int CalculateNextCellType()
+    {
+
+        return GetPriority();
+    }
+
+    public override void SpawnNextCell()
+    {
+
+    }
+
+
+}
