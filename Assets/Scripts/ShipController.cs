314fec08278e204e641dcad8a8a413c680b02e95
Artom Didinchuk <didinchukar@gmail.com>
2020-11-13 11:40:29 -0500

    adding ship

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] GameObject gun;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointGun();
+    }
+
+    private void pointGun()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = gun.transform.position - mousePos;
+        direction = direction / direction.magnitude;
+        float rotationZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        gun.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+}
