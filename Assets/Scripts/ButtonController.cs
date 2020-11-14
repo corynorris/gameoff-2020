a844d48c52c547a2aa77207758fab320314621c9
Andrew <adidinchuk@gmail.com>
2020-11-14 13:43:20 -0500

    added time control

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    private TurnManager turnManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turnManager = FindObjectOfType<TurnManager>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseClock()
+    {
+        turnManager.SetSpeed(0);
+    }
+
+    public void SetNormalSpeed()
+    {
+        turnManager.SetSpeed(1);
+    }
+
+    public void SetFastSpeed()
+    {
+        turnManager.SetSpeed(2);
+    }
+
+    public void SetMaxSpeed()
+    {
+        turnManager.SetSpeed(3);
+    }
+}
