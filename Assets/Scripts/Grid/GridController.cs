2cb63d118297f464511545020578610911e81c47
corynorris <corynorris@gmail.com>
2020-11-28 17:06:19 -0500

    made resouces auto-select, removed auto-play, moved clicks to actual cells

--- Assets/Scripts/Grid/GridController.cs
+++ Assets/Scripts/Grid/GridController.cs
@@ -14,11 +14,7 @@ public class GridController : MonoBehaviour
     public ResourceController ResourceController;
     public ShipController ship;
     private CellController[,] foregroundArray;
-    private static GridController _instance;
     private Vector3 bottomLeft;
-    private float offsetX;
-    private float offsetY;
-    private bool isFirst = true;
 
     public Dictionary<string, int> resourceTotals = new Dictionary<string, int>();
 
@@ -26,51 +22,13 @@ public class GridController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 fgPos = ForegroundCellObj.transform.position;
+        Vector3 bgPos = BackgroundCellObj.transform.position;
+        ForegroundCellObj.transform.SetPositionAndRotation(new Vector3(fgPos.x, fgPos.y, bgPos.z - 0.1f), ForegroundCellObj.transform.rotation);
         SoundManager.PlaySound(SoundManager.Sound.Background, turnManager.GetSpeed());
         turnManager.TurnPassed += RunSimulation;
         turnManager.Pause();
     }
-
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-            if (hit.collider != null)
-            {
-
-                CellController cell = hit.collider.gameObject.GetComponent<CellController>();
-                if (cell) {                    
-                
-                    if (foregroundArray[cell.X, cell.Y].IsClickable())
-                    {
-                        CellController activeResource = ResourceController.GetActiveResource();
-
-                        if (activeResource) {
-                            ship.FireGun();
-                            SpawnCell(cell.X, cell.Y, activeResource);
-                        }
-                        else
-                        {
-                            if (isFirst) {
-                                isFirst = false;
-                                turnManager.Resume();
-                            }
-
-                            SoundManager.PlaySound(SoundManager.Sound.CantPlace,turnManager.GetSpeed());
-                        }
-                    } else
-                    {
-                        SoundManager.PlaySound(SoundManager.Sound.CantPlace, turnManager.GetSpeed());
-                    }
-                } else
-                {
-                    SoundManager.PlaySound(SoundManager.Sound.Click, turnManager.GetSpeed());
-                }
-            }
-        }
-    }
-
 
     void Destroy()
     {
@@ -96,9 +54,6 @@ public class GridController : MonoBehaviour
             cell.X = x + (TilesWide / 2);
             cell.Y = y + (TilesHigh / 2) - 1;
 
-
-            //Debug.Log("Background: " + cell.ToString());
-
             if (x < bottomLeft.x)
             {
                 bottomLeft.x = x;
@@ -108,6 +63,8 @@ public class GridController : MonoBehaviour
             {
                 bottomLeft.y = y;
             }
+
+            AddBackgroundCellToGrid(cell);
         }
 
         foreach (CellController cell in foregroundCells)
@@ -118,7 +75,7 @@ public class GridController : MonoBehaviour
             cell.Y = y + (TilesHigh / 2) - 1;
 
         
-            AddCellToGrid(cell);
+            AddForegroundCellToGrid(cell);
         }
 
         foreach (CellController cell in backgroundCells)
@@ -129,154 +86,16 @@ public class GridController : MonoBehaviour
             }
         }
 
-  
-
-        //foreach (CellController foregroundCell in foregroundCells)
-        //{
-
-        //}
-
-        //if (_instance == null)
-        //{
-
-        //    _instance = this;
-        //    DontDestroyOnLoad(this.gameObject);
-        //    // _instance.turnManager.TurnPassed += RunSimulation;
-        //    ///////////////////////
-        //    _instance.backgroundArray = new CellController[TilesWide, TilesHigh];
-        //    _instance.foregroundArray = new CellController[TilesWide, TilesHigh];
-        //    ///////////////////////
-        //    List<CellController> backgroundCells = new List<CellController>(BackgroundCellObj.GetComponentsInChildren<CellController>());
-        //    List<CellController> foregroundCells = new List<CellController>(ForegroundCellObj.GetComponentsInChildren<CellController>());
+      }
 
 
-        //    //Debug.Log("Iterating over background cells");
-        //    //foreach (CellController backgroundCell in backgroundCells)
-        //    //{
-        //    //    Vector3Int roundedPosition = Vector3Int.RoundToInt(backgroundCell.transform.position);
-        //    //    Debug.Log(roundedPosition);
-        //    //    backgroundCell.transform.position = roundedPosition;
-
-        //    //}
-
-
-        //backgroundCells.Sort((p1, p2) =>
-        //{
-        //    Vector3 p1Pos = p1.transform.position;
-        //    Vector3 p2Pos = p2.transform.position;
-
-        //    if (p1Pos.y > p2Pos.y)
-        //    {
-        //        return 1;
-        //    }
-        //    else if (p1Pos.y < p2Pos.y)
-        //    {
-        //        return -1;
-        //    }
-        //    else
-        //    {
-        //        if (p1Pos.x > p2Pos.x)
-        //        {
-        //            return 1;
-        //        }
-        //        else if (p1Pos.x < p2Pos.x)
-        //        {
-        //            return -1;
-        //        }
-        //    }
-
-        //    return 0;
-
-
-        //});
-
-
-
-        //    int x = 0;
-        //    int y = 0;
-        //    float oldYPos = backgroundCells[0].transform.position.y;
-        //    _instance.offsetX = -backgroundCells[0].transform.position.x;
-        //    _instance.offsetY = -backgroundCells[0].transform.position.y;
-
-        //    foreach (CellController backgroundCell in backgroundCells)
-        //    {
-        //        //Debug.Log(backgroundCell.gameObject.transform.position);
-        //        if (backgroundCell.transform.position.y > oldYPos)
-        //        {
-        //            oldYPos = backgroundCell.transform.position.y;
-        //            y++;
-        //            x = 0;
-        //        }
-        //        backgroundCell.Y = y;
-        //        backgroundCell.X = x;
-
-        //        _instance.backgroundArray[x, y] = backgroundCell;
-        //        x++;
-
-        //    }
-
-        //    foreach (CellController backgroundCell in backgroundCells)
-        //    {
-        //        bool hasForeground = false;
-        //        foreach (CellController foregroundCell in foregroundCells)
-        //        {
-        //            if (foregroundCell.transform.position == backgroundCell.transform.position)
-        //            {
-        //                foregroundCell.X = backgroundCell.X;
-        //                foregroundCell.Y = backgroundCell.Y;
-        //                _instance.foregroundArray[backgroundCell.X, backgroundCell.Y] = foregroundCell;
-        //                hasForeground = true;
-        //                break;
-        //            }
-        //        }
-
-        //        if (!hasForeground)
-        //        {
-        //            SpawnCell(backgroundCell.X, backgroundCell.Y, EmptyCell);
-        //        }
-        //    }
-
-        //    foreach (CellController foregroundCell in foregroundCells)
-        //    {
-        //        foreach (CellController backgroundCell in backgroundCells)
-        //        {
-        //            if (foregroundCell.transform.position == backgroundCell.transform.position)
-        //            {
-        //                foregroundCell.X = backgroundCell.X;
-        //                foregroundCell.Y = backgroundCell.Y;
-        //                foregroundCell.Grid = _instance;
-
-        //                _instance.foregroundArray[backgroundCell.X, backgroundCell.Y] = foregroundCell;
-        //                break;
-        //            }
-        //        }
-        //    }
-
-        //    // Create and spawn empty tileset
-
-        //}
-        //else
-        //{
-        //    Destroy(this);
-        //}
-    }
-
-
-
-    public static GridController getInstance()
-    {
-        return _instance;
-    }
 
     public void Clear()
     {
-
         foreach (CellController child in this.BackgroundCellObj.GetComponentsInChildren<CellController>())      
         {
             DestroyImmediate(child.gameObject);
         }
-
-
     }
 
 
@@ -305,7 +124,6 @@ public class GridController : MonoBehaviour
         }
     }
 
-
     Vector2Int GetPointInDirection(int x, int y, GridDirection direction)
     {
         return new Vector2Int(x, y).MoveInGridDirection(direction);
@@ -324,14 +142,10 @@ public class GridController : MonoBehaviour
 
     }
 
-    // Can't write to cell directly because overwriting a cell might change the result of another cells calculation
-    // Can't use a simulated flag because a cell might be overwritten
-
     void RunSimulation(int turnsElapsed)
     {
         cellsToUpdate.Sort();
 
-        // Technically only need to recalculate for cells that changed and their neighbours
         for (int x = 0; x < TilesWide; x++)
         {
             for (int y = 0; y < TilesHigh; y++)
@@ -342,7 +156,6 @@ public class GridController : MonoBehaviour
 
         foreach (CellController cellController in cellsToUpdate)
         {
-           cellController.ProduceEffects();
            cellController.MakeClaims();
         }
 
@@ -383,10 +196,15 @@ public class GridController : MonoBehaviour
 
 
     }
+   
 
-    // Update is called once per frame
+    private void AddBackgroundCellToGrid(CellController cell)
+    {
+        cell.transform.SetParent(BackgroundCellObj.transform, false);
+        cell.Grid = this;
+    }
 
-    private void AddCellToGrid(CellController cell) {
+    private void AddForegroundCellToGrid(CellController cell) {
         cell.transform.SetParent(ForegroundCellObj.transform, false);
 
         cell.Grid = this;
@@ -399,8 +217,22 @@ public class GridController : MonoBehaviour
         }
     }
 
+    public void HandleCellClicked(CellController cell)
+    {
+        CellController activeResource = ResourceController.GetActiveResource();
 
-    public CellController SpawnCell(int x, int y, CellController prefab)
+        if (activeResource)
+        {
+            ship.FireGun();
+            SpawnCell(cell.X, cell.Y, activeResource);
+        }
+        else
+        {
+            SoundManager.PlaySound(SoundManager.Sound.CantPlace);
+        }
+    }
+
+    private CellController SpawnCell(int x, int y, CellController prefab)
     {
         if (x > TilesWide - 1 || y > TilesHigh - 1 || x < 0 || y < 0)
         {
@@ -430,7 +262,7 @@ public class GridController : MonoBehaviour
         cell.Y = y;
     
              
-        AddCellToGrid(cell);
+        AddForegroundCellToGrid(cell);
 
         return cell;
     }
--- Assets/Scripts/ResourceController.cs
+++ Assets/Scripts/ResourceController.cs
@@ -20,32 +20,11 @@ public class ResourceController : MonoBehaviour
     [Header("UI button frame for button deployment")]
     [SerializeField] GameObject buttonFrame;
 
-    [Header("UI button frame for gas display")]
-    [SerializeField] GameObject gasDisplayFrame;
-
-    [Header("Gas display prefab")]
-    [SerializeField] GameObject gasDisplayPrefab;
-
-    [Header("Gass statistics")]
-    [SerializeField] float Argon = 0.0f;
-    [SerializeField] float Helium = 0.0f;
-    [SerializeField] float Neon = 0.0f;
-    [SerializeField] float Oxygen = 0.0f;
-
-    [SerializeField] float ArgonChargeRate = 0.1f;
-    [SerializeField] float HeliumChargeRate = 0.2f;
-    [SerializeField] float NeonChargeRate = -0.1f;
-    [SerializeField] float OxygenChargeRate = 0.0f;
 
     private ShipController ship;
 
-    private float[] GasTotals = new float[GasHelpers.AllGasses.Length];
-
     SpawnButton[] spawnButtons;
-    GameObject[] gasDisplays;
     Transform[] buttonPositions;
-    Transform[] gasDisplayPositions;
-    float[] rechargeProgress;
 
     int activeResourceIndex = -1;
 
@@ -53,10 +32,6 @@ public class ResourceController : MonoBehaviour
 
     void Start()
     {
-        ProduceGas(Gas.Argon, Argon);
-        ProduceGas(Gas.Helium, Helium);
-        ProduceGas(Gas.Neon, Neon);
-        ProduceGas(Gas.Oxygen, Oxygen);
 
         activeResourceIndex = NextAvailableResource();
 
@@ -71,13 +46,9 @@ public class ResourceController : MonoBehaviour
         {
             _instance = this;
             //DontDestroyOnLoad(this.gameObject);
-            rechargeProgress = new float[GetButtonCount()];
             spawnButtons = new SpawnButton[GetButtonCount()];
-            gasDisplays = new GameObject[GetGasLength()];
             buttonPositions = buttonFrame.GetComponentsInChildren<Transform>();
-            gasDisplayPositions = gasDisplayFrame.GetComponentsInChildren<Transform>();
             InitializeButtons();
-            InitializeGasDisplay();
         }
         else
         {
@@ -85,17 +56,6 @@ public class ResourceController : MonoBehaviour
         }
     }
 
-    private void InitializeGasDisplay()
-    {
-
-        for (int displayIndex = 0; displayIndex < GetGasLength(); displayIndex++)
-        {
-            gasDisplays[displayIndex] = Instantiate(gasDisplayPrefab, gasDisplayPositions[displayIndex + 1].position, gasDisplayPositions[displayIndex + 1].rotation) as GameObject;
-            gasDisplays[displayIndex].transform.parent = gasDisplayFrame.transform;
-            gasDisplays[displayIndex].transform.localScale = gasDisplayPrefab.transform.localScale;
-            gasDisplays[displayIndex].transform.localPosition = gasDisplayPositions[displayIndex + 1].localPosition;
-        }
-    }
 
     private void InitializeButtons()
     {
@@ -104,12 +64,11 @@ public class ResourceController : MonoBehaviour
         {
 
             spawnButtons[buttonIndex] = Instantiate(buttonPrefab, buttonPositions[buttonIndex + 1].position, buttonPositions[buttonIndex + 1].rotation) as SpawnButton;
-            spawnButtons[buttonIndex].transform.parent = buttonFrame.transform;
+            spawnButtons[buttonIndex].transform.SetParent(buttonFrame.transform);
             spawnButtons[buttonIndex].transform.localScale = buttonPrefab.transform.localScale;
             spawnButtons[buttonIndex].transform.localPosition = buttonPositions[buttonIndex + 1].localPosition;
             spawnButtons[buttonIndex].setResourceCount(resourceCount[buttonIndex]);
             spawnButtons[buttonIndex].setResourceMax(resourceMax[buttonIndex]);
-            spawnButtons[buttonIndex].setRechargeProgress(rechargeProgress[buttonIndex]);
             spawnButtons[buttonIndex].setIndex(buttonIndex);
             spawnButtons[buttonIndex].setImage(buttonSprites[buttonIndex]);
             spawnButtons[buttonIndex].setTooltipText(tooltipText[buttonIndex]);
@@ -117,22 +76,11 @@ public class ResourceController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame ... this is for physics wat wat
-    void FixedUpdate()
-    {
-        IncrementResourceCounts(1);
-
-    }
-
     private int GetButtonCount()
     {
         return resourceCount.Length;
     }
 
-    private int GetGasLength()
-    {
-        return GasHelpers.AllGasses.Length;
-    }
 
     public static ResourceController getInstance()
     {
@@ -144,7 +92,7 @@ public class ResourceController : MonoBehaviour
     public int NextAvailableResource()
     {
         int t = activeResourceIndex > 0 ? activeResourceIndex : 0;
-        spawnButtons[t].setActive(false);
+    
 
         for (int i = 0; i < resourceCount.Length; i++)
         {
@@ -162,8 +110,7 @@ public class ResourceController : MonoBehaviour
     }
 
     public void SetActiveResource(int index)
-    {
-        
+    {        
         activeResourceIndex = index;
         for (int buttonIndex = 0; buttonIndex < GetButtonCount(); buttonIndex++)
         {
@@ -172,7 +119,9 @@ public class ResourceController : MonoBehaviour
             if (buttonIndex == activeResourceIndex)
                 spawnButtons[buttonIndex].setActive(true);
         }
-        ship.LoadGun(resourcePrefabs[activeResourceIndex]);
+        if (activeResourceIndex >= 0) { 
+            ship.LoadGun(resourcePrefabs[activeResourceIndex]);
+        } 
     }
 
     public void ClearActiveResource()
@@ -182,6 +131,7 @@ public class ResourceController : MonoBehaviour
 
     public CellController GetActiveResource()
     {
+
         activeResourceIndex = NextAvailableResource();
 
         if (activeResourceIndex >= 0)
@@ -192,58 +142,17 @@ public class ResourceController : MonoBehaviour
                 resourceMax[activeResourceIndex] = resourceMax[activeResourceIndex] - 1;
                 spawnButtons[activeResourceIndex].setResourceCount(resourceCount[activeResourceIndex]);
                 spawnButtons[activeResourceIndex].setResourceMax(resourceMax[activeResourceIndex]);
-                return resourcePrefabs[activeResourceIndex];
+
+                int prevActiveResourceIndex = activeResourceIndex;
+                SetActiveResource(NextAvailableResource());
+                return resourcePrefabs[prevActiveResourceIndex];
             }
         }
+
+        
 
         return null;
     }
-    void IncrementResourceCounts(int turnsElapsed)
-    {        
-        if (spawnButtons.Length > 0)
-        {
-            for (int buttonIndex = 0; buttonIndex < GetButtonCount(); buttonIndex++)
-            {
-                if (resourceCount[buttonIndex] != resourceMax[buttonIndex])
-                {
-                    rechargeProgress[buttonIndex] = rechargeProgress[buttonIndex] + resourceChargeRate[buttonIndex] * Time.deltaTime;
-                    if (rechargeProgress[buttonIndex] >= 1)
-                    {
-                        resourceCount[buttonIndex]++;
-                        rechargeProgress[buttonIndex] = 0;
-                    }
-                    spawnButtons[buttonIndex].setResourceCount(resourceCount[buttonIndex]);
-                    spawnButtons[buttonIndex].setResourceMax(resourceMax[buttonIndex]);
-                    spawnButtons[buttonIndex].setRechargeProgress(rechargeProgress[buttonIndex]);
+    
 
-                }
-            }
-        }
-
-        ConsumeGas(Gas.Argon, ArgonChargeRate * Time.deltaTime);
-        ConsumeGas(Gas.Helium, HeliumChargeRate * Time.deltaTime);
-        ConsumeGas(Gas.Neon, NeonChargeRate * Time.deltaTime);
-        ConsumeGas(Gas.Oxygen, OxygenChargeRate * Time.deltaTime);
-
-        for (int displayIndex = 0; displayIndex < GasHelpers.AllGasses.Length; displayIndex++)
-        {
-            gasDisplays[displayIndex].GetComponent<Text>().text = ((Gas)displayIndex).ToString() + " : " + Math.Round(GetTotalGas((Gas)displayIndex),1);
-        }
-
-    }
-
-    public void ProduceGas(Gas gas, float amount)
-    {
-        GasTotals[(int)gas] = Mathf.Max(GasTotals[(int)gas] + amount, 0);
-    }
-
-    public void ConsumeGas(Gas gas, float amount)
-    {
-        GasTotals[(int)gas] = Mathf.Max(GasTotals[(int)gas] - amount, 0);
-    }
-
-    public float GetTotalGas(Gas gas)
-    {
-        return GasTotals[(int)gas];
-    }
 }
