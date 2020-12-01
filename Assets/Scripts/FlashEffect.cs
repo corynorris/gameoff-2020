c35908883a6331f7a555da02d026b59f1e1e27ab
corynorris <corynorris@gmail.com>
2020-12-01 16:33:36 -0500

    Added flash effect

--- Assets/Scripts/FlashEffect.cs
+++ Assets/Scripts/FlashEffect.cs
@@ -6,44 +6,74 @@ using UnityEngine.UI;
 public class FlashEffect : MonoBehaviour
 {
     public Image image;
-    public float interval = 1f;
+    public float interval = 0.4f;
     public Color flashColor;
+    private Color originalColor;
+    private Color targetColor;
 
     private TurnManager turnManager;
     private ResourceController resourceController;
 
 
-    private bool toggleFlashing = false;
+    private bool isFlashing = false;
     // Start is called before the first frame update
     void Start()
     {
         turnManager = FindObjectOfType<TurnManager>();
         resourceController = FindObjectOfType<ResourceController>();
         resourceController.HopperEmpty += BeginFlash;
+        turnManager.TurnPassed += EndFlash;
     }
 
     private void BeginFlash()
     {
-        Debug.Log("Begin Flash");
+        isFlashing = true;
+        originalColor = image.color;
         StopAllCoroutines();
         StartCoroutine("Flash");
     }
 
-    IEnumerator Flash()
+    private void EndFlash(int time)
     {
-        Debug.Log("Flash");
-        image.color = flashColor;
-        yield return new WaitForSeconds(interval);
+        if (isFlashing) {
+            isFlashing = false;
+            image.color = originalColor;
+            StopAllCoroutines();
+        }
     }
 
-    private void OnMouseDown()
+    IEnumerator Flash()
     {
-        StopAllCoroutines();
+        while (isFlashing) {
+            
+            float t = 0;
+
+            Color startColor = image.color;
+
+            if (image.color == flashColor)
+            {
+                targetColor = originalColor;
+            }
+            else
+            {
+                targetColor = flashColor;
+            }
+
+            while (t < interval)
+            {
+                t += Time.deltaTime;
+                image.color = Color.Lerp(startColor, targetColor, t / interval);
+                yield return null;
+            }
+
+        }
     }
+
 
     void Destroy()
     {
         resourceController.HopperEmpty -= BeginFlash;
+        turnManager.TurnPassed -= EndFlash;
     }
 
 
